fb309588bc2e2da9250b3c9f55e01373ad71d24c
Lapidis2 <hitlapidis2@gmail.com>
2026-03-23 14:37:55 +0200

    updating search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,11 +1,42 @@
-import { View, Text } from 'react-native'
+import { View,Image, FlatList } from 'react-native'
 import React from 'react'
+import useFetch from '@/services/useFetch'
+import { getMovies } from '@/services/api'
+
+import { images } from '@/constants/images'
+import MovieCard from '@/components/MovieCard'
 
 const Search = () => {
+   const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    getMovies({
+      query: "",
+    }),
+  );
   return (
-      <View className='flex justify-center items-center bg-primary h-screen'>
-           <Text className='text-white text-lg font-bold'> Search! </Text>
-        
+      <View className='flex-1 bg-primary'>
+           <Image source={images.bg} className='w-full flex-1 absolute z-0' resizeMode='cover' />
+      
+<FlatList
+data={movies}
+keyExtractor={(item)=>item.id.toString()}
+renderItem={({item})=><MovieCard {...item} />}
+numColumns={3}
+className='px-5'
+columnWrapperStyle={
+  {
+    justifyContent:'center',
+    gap:10,
+    marginTop:15
+  }
+}
+
+contentContainerStyle={{paddingBottom:100, minHeight:'100%'}}
+/>
+
          </View>
   )
 }
